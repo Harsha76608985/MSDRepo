721f65c0957cca1329d98bf3abbcf3761fbeb2c5
Harsha76608985 <100669444+Harsha76608985@users.noreply.github.com>
2022-03-03 16:13:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
   public static void main(String[] args){
    System.out.println("hello Jenkins i am from github...nice to see u!!!");
+    int a=8;
+    System.out.println("hii Iam Harsha"); 
   }
 }
   
